5dcb34ecd54adec9d9fee6732e8027a6d9b2e7dc
Prashant Lalwani <99117462+PrashantLalwani8289@users.noreply.github.com>
2024-07-02 17:00:54 +0530

    Completes the frontend setup for the speech to text converter

---
+++ src/components/AudioInput.tsx
@@ -0,0 +1,65 @@
+'use client';
+import axios from 'axios';
+import { useState, ChangeEvent, FormEvent } from 'react';
+
+const AudioInput = () => {
+    const [file, setFile] = useState<File | null>(null);
+    const [transcription, setTranscription] = useState<string>("");
+    const [error, setError] = useState<string>("");
+
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = event.target.files?.[0] || null;
+        setFile(selectedFile);
+    };
+
+    const convertToBase64 = (file: File): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(reader.result as string);
+            reader.onerror = error => reject(error);
+        });
+    };
+
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+
+        if (!file) {
+            setError("Please select a file to upload.");
+            return;
+        }
+
+        try {
+            const base64String = await convertToBase64(file);
+            // console.log(base64String);
+            const response = await axios.post("http://localhost:3002/transcript-audio", { audio: base64String }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }); 
+
+            if (response.status !== 200) {
+                throw new Error(`Error: ${response.statusText}`);
+            }
+            console.log(response.data.transcription)
+            setTranscription(response.data.transcription);
+            setError("");
+        } catch (error: any) {
+            setError(error.message);
+            setTranscription("");
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="file" accept="audio/*" onChange={handleFileChange} />
+                <button type="submit">Upload and Transcribe</button>
+            </form>
+            {transcription && <div><strong>Transcription:</strong> <p>{transcription}</p></div>}
+            {error && <div><strong>Error:</strong> <p>{error}</p></div>}
+        </div>
+    );
+};
+
+export default AudioInput;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='py-2 flex items-center h-6 border-emerald-600'>
+        <div className='flex-1 flex items-center justify-center sm:justify-start text-2 border-black'>
+            Speech to Text Converter
+        </div>
+        
+    </nav>
+  )
+}
+
+export default Navbar
